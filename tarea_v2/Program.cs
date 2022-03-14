f98db2caaa0dcb856c4d114d8797a3c82fae1ffd
Israel García Agüero <ig239503@gmail.com>
2022-03-13 21:34:23 -0600

    id

--- tarea_v2/Program.cs
+++ tarea_v2/Program.cs
@@ -9,10 +9,10 @@ namespace tarea_v2
         static void Main(String[] args)
         {
             Peliculas c1 = new Peliculas(1, "Terror", "VIERNES TRECE", "VIERNES TRECE 5");
-            Peliculas c2 = new Peliculas(1, "Ciencia Ficcion", "STAR WARS", "EL RETORNO DEL JEDAI");
-            Peliculas c3 = new Peliculas(1, "Fantasia", "HARRY POTTER", "HARRY POTTER Y LA PIEDRA FILOSOFAL");
-            Peliculas c4 = new Peliculas(1, "Terror", "VIERNES TRECE", "VIERNES TRECE");
-            Peliculas c5 = new Peliculas(1, "Terror", "VIERNES TRECE", "VIERNES TRECE 2");
+            Peliculas c2 = new Peliculas(2, "Ciencia Ficcion", "STAR WARS", "EL RETORNO DEL JEDAI");
+            Peliculas c3 = new Peliculas(3, "Fantasia", "HARRY POTTER", "HARRY POTTER Y LA PIEDRA FILOSOFAL");
+            Peliculas c4 = new Peliculas(4, "Terror", "VIERNES TRECE", "VIERNES TRECE");
+            Peliculas c5 = new Peliculas(5, "Terror", "VIERNES TRECe", "VIERNES TRECE 2");
 
         }
     }
