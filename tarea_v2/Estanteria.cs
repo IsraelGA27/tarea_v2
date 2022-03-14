7069ef94de60d15b0cd1505d267589240fa68913
Israel García Agüero <ig239503@gmail.com>
2022-03-13 21:32:34 -0600

    Cambios extraños que hay en mi

--- tarea_v2/Estanteria.cs
+++ tarea_v2/Estanteria.cs
@@ -8,5 +8,66 @@ namespace tarea_v2
 {
     internal class Estanteria
     {
+        private Peliculas[] _pelis;
+        private int _limite;
+
+        private int _numPelis;
+
+        public Estanteria(int limite)
+        {
+            this._pelis = new Peliculas[limite];
+            _limite = limite; 
+            _numPelis = 0; // Valores en el constructor (por paradigma)
+        }
+
+        public void anadirPelicula(Peliculas c)
+        {
+            if (c != null && _numPelis < _pelis.Length)
+            {
+                _pelis[_numPelis] = c;
+                _numPelis++;
+            }
+        }
+        // Mostrar 'info' de las peliculas (para especificar )
+        public void mostrarPelicula()
+        {
+            for (int i=0; i < _numPelis; i++)
+            {
+                Console.WriteLine(_pelis[i].ToString());
+            }
+        }
+        public void vaciarPeliculas()
+        {
+            this._pelis = new Peliculas[_limite];
+        }
+        public void eliminarPeliculas(Peliculas c)
+        {
+            if (c!= null && _numPelis != 0)
+            {
+                int posicion = -1;
+                for (int i = 0; i < _pelis.Length; i++)
+                {
+                    if(c.id == _pelis[i].id)
+                    {
+                        posicion = i;
+                    }
+                }
+                if (posicion == -1)
+                {
+                    Console.WriteLine("No Existe la pelicula");
+                }
+                else
+                {
+                    _pelis[posicion] = null;
+
+                    for (int i = posicion; i < _numPelis; i++)
+                    {
+                        _pelis[i] = _pelis[i + 1];
+                    }
+
+                    _numPelis--;
+                }
+            }
+        }
     }
 }
--- tarea_v2/Program.cs
+++ tarea_v2/Program.cs
@@ -2,4 +2,18 @@
 // Console.WriteLine("TAREA 2");
 
 
+namespace tarea_v2
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Peliculas c1 = new Peliculas(1, "Terror", "VIERNES TRECE", "VIERNES TRECE 5");
+            Peliculas c2 = new Peliculas(1, "Ciencia Ficcion", "STAR WARS", "EL RETORNO DEL JEDAI");
+            Peliculas c3 = new Peliculas(1, "Fantasia", "HARRY POTTER", "HARRY POTTER Y LA PIEDRA FILOSOFAL");
+            Peliculas c4 = new Peliculas(1, "Terror", "VIERNES TRECE", "VIERNES TRECE");
+            Peliculas c5 = new Peliculas(1, "Terror", "VIERNES TRECE", "VIERNES TRECE 2");
 
+        }
+    }
+}
